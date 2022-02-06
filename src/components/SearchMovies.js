bceadc44de97f36ab3c4816379b0a43fe4c5e7c3
Sharma <shyamsharma.sharma797@gmail.com>
2022-02-05 22:55:44 -0600

    Only commit

---
+++ src/components/SearchMovies.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+import Card from './Card';
+
+const SearchMovies = () => {
+
+    const [query, setQuery] = useState('');
+
+    const [movies, setMovies]= useState([]);
+
+    const searchMovies = async (e) =>{
+        e.preventDefault();
+        
+     try
+     {   
+        const res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=e624ba2f472b087992f7154919491239&query=${query}`);
+        const data = await res.json();
+        setMovies(data.results);
+    }catch(err){
+        console.error(err);
+    }
+}
+const elements = movies.map(movie => <Card movie = {movie} key = {movie.id}/>)
+  return <div>
+      <form className='form' onSubmit={searchMovies}>
+          <label className='label' htmlFor='query'>Movie Name</label>
+          <input className='input' type='text' name='query'
+                placeholder='i.e Jurassic Park'
+                value = {query}
+                onChange = {(e) => setQuery(e.target.value)}
+                />
+        <button className='button' type='submit'>Search</button>
+      </form>
+      {elements}
+  </div>;
+};
+
+export default SearchMovies;
